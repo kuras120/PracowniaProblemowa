579fbc46178aec4d0523888ad2433a3c97383fbb
Wojciech Kur <wojciech.kur96@gmail.com>
2022-05-11 00:33:32 +0200

    Test data for knn

--- knn/test.py
+++ knn/test.py
@@ -1,13 +1,36 @@
-import numpy as np
-import sys
 import pickle
+import pandas as pd
+
+from knn.train import ddos_features
 
 
-def ddos_test(attributes):
+def ddos_test():
     model = pickle.load(open("./saved_model/ddos.sav", 'rb'))
-    result = model.predict([attributes])
-    print(result)
+    df1 = pd.read_csv('./knn_tuples_test.csv')
+    X = df1.loc[:, ddos_features]
+    it, acc, fp, fn = 0, 0, 0, 0
+    for row in X.itertuples():
+        list_row = list(row)
+        list_row.pop(0)
+        print(list_row)
+        predicted = model.predict([list_row])
+        actual = df1.iloc[[it]].label.values
+        print(actual)
+        print('Predicted: ', predicted, 'Actual: ', actual)
+        if predicted == actual:
+            acc += 1
+        elif predicted > actual:
+            fp += 1
+        else:
+            fn += 1
+        it += 1
+    print('ACC: ', acc, ' FP: ', fp, ' FN: ', fn)
+    f1_score = 2 * acc / (2 * acc + fp + fn)
+    print(len(df1))
+    acc = (acc / len(df1)) * 100
+    print('Accuracy: ', acc)
+    print('F1 score: ', f1_score)
 
 
 if __name__ == "__main__":
-    ddos_test(sys.argv[2:])
+    ddos_test()
--- knn/train.py
+++ knn/train.py
@@ -1,26 +1,39 @@
+import os
 import numpy as np
 import pandas as pd
 import pickle
 from sklearn.neighbors import KNeighborsClassifier
 
+ddos_features = ['src_ip_addr', 'src_port', 'dst_ip_addr', 'dst_port', 'protocol', 'f1', 'f2', 'f3', 'f4', 'f5',
+                 'f6', 'f7', 'f8', 'f9', 'f10']
+
 
 def train_ddos():
-    df1 = pd.read_csv('./knn_tuples.csv')
-    ddos_features = ['src_port', 'dst_port', 'protocol', 'f1', 'f2', 'f3', 'f4', 'f5',
-                     'f6', 'f7', 'f8', 'f9', 'f10']
+    if not os.path.exists('./knn_tuples_learn.csv') or not os.path.exists('./knn_tuples_test.csv'):
+        df1 = pd.read_csv('./knn_tuples.csv')
+        df1['src_ip_addr'] = df1['src_ip_addr'].map(lambda x: x.replace('.', '')).astype(int)
+        df1['dst_ip_addr'] = df1['dst_ip_addr'].map(lambda x: x.replace('.', '')).astype(int)
+        # 88 % of all rows
+        learn = pd.concat([df1.iloc[:14000, :], df1.iloc[16395:40000, :]]).sample(frac=1).reset_index(drop=True)
+        learn.to_csv('./knn_tuples_learn.csv')
+        # 12 % of all rows
+        test = pd.concat([df1.iloc[14000:16395, :], df1.iloc[40000:42721]]).sample(frac=1).reset_index(drop=True)
+        test.to_csv('./knn_tuples_test.csv')
+    else:
+        learn = pd.read_csv('./knn_tuples_learn.csv')
     ddos_target = 'label'
-    X = df1.loc[:, ddos_features]
-    y = df1.loc[:, ddos_target]
+    X = learn.loc[:, ddos_features]
+    y = learn.loc[:, ddos_target]
     classes = np.unique(y)
     for i in range(len(classes)):
         if i == 2:
-            df1 = df1.replace(classes[i], 0)
+            learn = learn.replace(classes[i], 0)
         else:
-            df1 = df1.replace(classes[i], 1)
+            learn = learn.replace(classes[i], 1)
     print("Data preprocessing done.")
     model = KNeighborsClassifier(n_neighbors=3)
     # fitting our model
-    model.fit(X, y)
+    model.fit(X.values, y)
     print("The model has been fit.")
 
     print("Save the fitted model?(y/n):")
